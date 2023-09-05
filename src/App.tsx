4405c6788dfccc3072eebed9fb7583a9f17af8ef
Albin Lundström <albin.wilhelm.erik@gmail.com>
2023-09-05 07:14:13 +0200

    testing click

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
  
+const [click,setClick]= useState(0)
 
   return (
     <>
+    {click}
       <h1>Portolio</h1>
       <h2>Under construction...</h2>
+      <button onClick={()=>setClick(prev=>prev+1)}> Increment</button>
     </>
   )
 }
